89cd6850e53ba3a828510a7c5629a837ee8abe80
julienz5 <julien.zana5@gmail.com>
2019-07-05 16:40:02 +0200

    a
    a

--- sendemail.php
+++ sendemail.php
@@ -4,30 +4,35 @@
 define( "RECIPIENT_NAME", "CONTACT avisplus" );
 define( "RECIPIENT_EMAIL", "contact@avisplus.fr" );
 
-
 // Read the form values
 $success = false;
-$pname = isset( $_POST['name'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['name'] ) : "";
-$busname = isset( $_POST['busname'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['busname'] ) : "";
-$topic = isset( $_POST['topic'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['topic'] ) : "";
-$senderEmail = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['email'] ) : "";
-$senderPhone = isset( $_POST['phone'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['phone'] ) : "";
-$body = isset( $_POST['descr'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['descr'] ) : "";
 
-// If all values exist, send the email
-if ( $pname && $busname && $topic && $senderEmail && $senderPhone) {
-  $recipient = RECIPIENT_NAME . " <" . RECIPIENT_EMAIL . ">";
-  $headers = "From: " . $senderEmail . " <" . $busname . ">";
-  $msgBody = " Nom contact: " . $pname . " Business name: " . $busname . " Topic: " . $topic . " Phone Number: " . $senderPhone . "" . " Body: " . $body . "";
-  $success = mail( $recipient, $headers, $msgBody );
+$pname = htmlspecialchars( $_POST['pname'] );
+$busname = htmlspecialchars( $_POST['busname'] );
+$topic = htmlspecialchars( $_POST['topic'] );
+$senderEmail = htmlspecialchars( $_POST['email'] );
+$senderPhone = htmlspecialchars( $_POST['phone'] );
+$body = htmlspecialchars( $_POST['descr'] );
 
-  //Set Location After Successsfull Submission
-  header('Location: index.html?message=Successfull');
+// If necessary values exist, send the email
+if ($pname && $busname && $senderEmail)
+{
+	$recipient = RECIPIENT_NAME . " <" . RECIPIENT_EMAIL . ">";
+	$headers = "From: " . $senderEmail . " <" . $busname . ">";
+	$msgBody = " Nom contact: " . $pname . " Business name: " . $busname . " Topic: " . $topic . " Phone Number: " . $senderPhone . "" . " Body: " . $body . "";
+	$success = mail( $recipient, $headers, $msgBody );
+
+	// If necessary values exist, send the email
+	if ($success)
+	{
+		header('Location: index.html?message=Success');
+	}
+	else{
+		header('Location: index.html?message=Failed');
+	}
 }
-
 else{
-	//Set Location After Unsuccesssfull Submission
-  	header('Location: index.html?message=Failed');	
+  	header('Location: index.html?message=Invalid');
 }
 
 ?>
